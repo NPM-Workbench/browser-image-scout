cd09d33e84b48032d35448dbf457c137ad48e246
sricharankrishnan <sricharan.krishnan@gmail.com>
2026-05-14 15:04:19 +0530

    [feat] PNG Support
    - supportsJPEG created to help check for png mime type support, which
      is a promise based resolution
    - small updates to the jpeg file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 /* exports */
 export * from "./types/index.js";
 export * from "./supports-jpeg/index.js";
+export * from "./supports-png/index.js";
--- src/supports-png/index.ts
+++ src/supports-png/index.ts
@@ -3,21 +3,21 @@ import { BASE_64_IMG_ENC } from "../shared/index.js";
 import type { TMimeType, TImageType } from "../types/index.js";
 
 /* types */
-type TSupportsJPEGReturn = {
+type TSupportsPNGReturn = {
   mimeType: TMimeType,
   imgType: TImageType,
   supported: boolean
 };
 
 /* module */
-async function supportsJPEG(): Promise<TSupportsJPEGReturn> {
+async function supportsPNG(): Promise<TSupportsPNGReturn> {
   if (typeof window === "undefined") {
     throw new Error("[Global Not Found]: Window Object");
   } else  {
     return new Promise((resolve) => {
       /* setup */
-      const mimeType = "image/jpeg";
-      const imgType = "jpeg";
+      const mimeType = "image/png";
+      const imgType = "png"
       const B64_URL = `data:${mimeType};base64,${BASE_64_IMG_ENC}`;
       const image = new Image();
       image.src = B64_URL;
@@ -36,5 +36,5 @@ async function supportsJPEG(): Promise<TSupportsJPEGReturn> {
 }
 
 /* exports */
-export type { TSupportsJPEGReturn };
-export { supportsJPEG };
+export type { TSupportsPNGReturn };
+export { supportsPNG };
