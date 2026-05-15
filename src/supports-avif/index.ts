04fc371acda3ac925ba50b729a6be1b937eb11f7
sricharankrishnan <sricharan.krishnan@gmail.com>
2026-05-15 10:56:25 +0530

    [feat] AVIF Support
    - supportsAVIF created to help check for avif mime type support, which
      is a promise based resolution

---
+++ src/supports-avif/index.ts
@@ -0,0 +1,41 @@
+/* imports */
+import { AVIF_BASE_64_IMG_ENC } from '../shared/index.js';
+import type { TMimeType, TImageType } from '../types/index.js';
+
+/* types */
+type TSupportsAVIFReturn = {
+  mimeType: TMimeType;
+  imgType: TImageType;
+  supported: boolean;
+};
+
+/* module */
+async function supportsAVIF(): Promise<TSupportsAVIFReturn> {
+  if (typeof window === 'undefined') {
+    throw new Error('[Global Not Found]: Window Object');
+  } else if (typeof Image === 'undefined') {
+    throw new Error('[Global Not Found]: Image Constructor');
+  } else {
+    return new Promise((resolve) => {
+      /* setup */
+      const mimeType = 'image/avif';
+      const imgType = 'avif';
+      const B64_URL = `data:${mimeType};base64,${AVIF_BASE_64_IMG_ENC}`;
+      const image = new Image();
+
+      image.addEventListener('load', () => {
+        resolve({ mimeType, imgType, supported: image.naturalWidth > 0 });
+      }, { once: true });
+
+      image.addEventListener('error', () => {
+        resolve({ mimeType, imgType, supported: false });
+      }, { once: true });
+
+      image.src = B64_URL;
+    });
+  }
+}
+
+/* exports */
+export type { TSupportsAVIFReturn };
+export { supportsAVIF };
