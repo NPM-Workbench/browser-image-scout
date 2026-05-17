b317c7de5aeffbd639f95dbd9924134ec9c24f5f
sricharankrishnan <sricharan.krishnan@gmail.com>
2026-05-17 09:29:15 +0530

    [feat] Unit Tests
    - created unit test cases for:
      Supports PNG
      1. throws when the window global is not available
      2. throws when the Image constructor is not available
      3. resolves "support = true" when PNG is supported
      4. resolves "support = false" when PNG is not supported

---
+++ src/supports-png/tests/supports-png.test.ts
@@ -0,0 +1,63 @@
+/* imports */
+import { supportsPNG } from '../index.js';
+import type { TSupportsPNGReturn } from '../index.js';
+import { MockImage } from '../../shared/mocks/mock-image.js';
+import { MockBrokenImage } from '../../shared/mocks/mock-broken-image.js';
+
+/* suite */
+describe('Supports PNG', () => {
+  /* setup */
+  const originalWindow = globalThis.window;
+  const originalImage = globalThis.Image;
+
+  /* before-each: life cycle */
+  beforeEach(() => {
+    globalThis.window = {} as any;
+    globalThis.Image = MockImage as any;
+  });
+
+  /* after-each: life cycle */
+  afterEach(() => {
+    globalThis.window = originalWindow;
+    globalThis.Image = originalImage;
+  });
+
+  /* 1 */
+  test('throws when the window global is not available', async () => {
+    globalThis.window = undefined as any;
+    await expect(supportsPNG()).rejects.toThrow(
+      '[Global Not Found]: Window Object',
+    );
+  });
+
+  /* 2 */
+  test('throws when the Image constructor is not available', async () => {
+    globalThis.Image = undefined as any;
+    await expect(supportsPNG()).rejects.toThrow(
+      '[Global Not Found]: Image Constructor',
+    );
+  });
+
+  /* 3 */
+  test('resolves "support = true" when PNG is supported', async () => {
+    /* setup */
+    const response: TSupportsPNGReturn = await supportsPNG();
+
+    /* assert */
+    expect(response.supported).toBe(true);
+    expect(response.mimeType).toBe('image/png');
+    expect(response.imgType).toBe('png');
+  });
+
+  /* 4 */
+  test('resolves "support = false" when PNG is not supported', async () => {
+    /* setup */
+    globalThis.Image = MockBrokenImage as any;
+    const response: TSupportsPNGReturn = await supportsPNG();
+
+    /* assert */
+    expect(response.supported).toBe(false);
+    expect(response.mimeType).toBe('image/png');
+    expect(response.imgType).toBe('png');
+  });
+});
